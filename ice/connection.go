7743882ba5acf8b5fafb05fcbcd0908064e507a1
aloxc <leerohwa@gmail.com>
2019-04-24 22:10:49 +0800

    重新写了一个连接池，性能大幅提升，

--- ice/connection.go
+++ ice/connection.go
@@ -1,11 +1,7 @@
 package ice
 
 import (
-	"bufio"
 	"errors"
-	"fmt"
-	"github.com/aloxc/goice/config"
-	"github.com/aloxc/goice/utils"
 	"github.com/siddontang/go-log/log"
 	"github.com/siddontang/go/sync2"
 	"net"
@@ -19,62 +15,46 @@ var (
 	hasMoreError  = errors.New("没有可用连接了")
 	waitMoreError = errors.New("等待请求过多")
 )
-type Connection struct {
-	*net.TCPConn
-}
-type connAndError struct {
-	*Connection
-	error
-}
 
-//连接超时monitor
-func connectTimeoutMonitor(network string, address string, timeout int, caech chan *connAndError) {
-	log.Info("启动连接监控启动", address)
-	<-time.After(time.Duration(timeout) * time.Second)
-	caech <- &connAndError{
-		error: NewConnectTimeoutError(address, network, timeout),
-	}
-	log.Info("超时完成")
-}
-func connect(network string, address string, caech chan *connAndError) {
-	//log.Info("准备连接服务器",address)
-	var remoteAddress, _ = net.ResolveTCPAddr(network, address)
-	var conn, err = net.DialTCP(network, nil, remoteAddress)
-	if err != nil {
-		caech <- &connAndError{
-			error: err,
-		}
-		return
-	}
-	c := &Connection{
-		TCPConn: conn,
-	}
-	c.init()
-	caech <- &connAndError{
-		Connection: c,
-	}
-	//log.Info("已连接服务器",address)
-	return
-}
+//func connect(network string, address string, caech chan *connAndError) {
+//	//log.Info("准备连接服务器",address)
+//	var remoteAddress, _ = net.ResolveTCPAddr(network, address)
+//	var conn, err = net.DialTCP(network, nil, remoteAddress)
+//	if err != nil {
+//		caech <- &connAndError{
+//			error: err,
+//		}
+//		return
+//	}
+//	c := &Connection{
+//		TCPConn: conn,
+//	}
+//	c.init()
+//	caech <- &connAndError{
+//		Connection: c,
+//	}
+//	//log.Info("已连接服务器",address)
+//	return
+//}
 
 //like this network:tcp,address :127.0.0.1:1888
-func Connect(network string, address string, timeout int) (*Connection, error) {
-	var caech = make(chan *connAndError)
-	go connect(network, address, caech)
-	go connectTimeoutMonitor(network, address, timeout, caech)
-	var cae *connAndError = <-caech
-	return cae.Connection, cae.error
-	//var remoteAddress, _ = net.ResolveTCPAddr(network, address)
-	//var conn, err = net.DialTCP(network, nil, remoteAddress)
-	//if err != nil {
-	//	return nil, err
-	//}
-	//c := &Connection{
-	//	TCPConn: conn,
-	//}
-	//c.init()
-	//return c, nil
-}
+//func Connect(network string, address string, timeout int) (*Connection, error) {
+//	var caech = make(chan *connAndError)
+//	go connect(network, address, caech)
+//	go connectTimeoutMonitor(network, address, timeout, caech)
+//	var cae *connAndError = <-caech
+//	return cae.Connection, cae.error
+//	//var remoteAddress, _ = net.ResolveTCPAddr(network, address)
+//	//var conn, err = net.DialTCP(network, nil, remoteAddress)
+//	//if err != nil {
+//	//	return nil, err
+//	//}
+//	//c := &Connection{
+//	//	TCPConn: conn,
+//	//}
+//	//c.init()
+//	//return c, nil
+//}
 
 type NewConnHook interface {
 	hook(conn *net.Conn) error
@@ -187,171 +167,94 @@ func (this *Pool) Close() {
 	this.usingConns = nil
 	log.Info("关闭完毕")
 }
-
-
-
-func InitConnection(identity *Identity, name string, conn *net.Conn) {
-	//log.Info("连接后要发送一条head命令")
-	var facet string
-	rw := bufio.NewReadWriter(bufio.NewReader(*conn), bufio.NewWriter(*conn))
-	var buf = NewIceBuff(rw)
-
-	total, real := PrepareHead(identity, "", config.ConfigMap[name][config.Module].(string), nil)
-	//log.Infof("name[%s]连接池创建好了%d %d %s %s",name,total,real,config.ConfigMap[name][config.Module].(string),identity.name)
-
-	var context map[string]string
-
-	var size int
-	var head, data []byte
-	var err error
-	buf.Write(*GetConnHead())          // 10字节
-	buf.Write(utils.IntToBytes(total)) //size 10 +4 = 14
-	buf.Write(utils.IntToBytes(1))     //requestId 14+4=18
-	buf.WriteStr(identity.name)        //18+1+8=27
-	buf.WriteStr(identity.category)    //27+1=28
-
-	if len(facet) == 0 {
-		buf.WriteByte(0) //28+1=29
-	} else {
-		facets := []string{facet}
-		buf.WriteStringArray(facets)
-	}
-	buf.WriteStr(string(config.Ice_isA))         //29+1+7=37
-	buf.WriteByte(byte(OperatorModeNonmutating)) //37+1=38
-	context = make(map[string]string)
-	buf.WriteStringMap(context) //38+1=39
-	//数据整形 java 中 BasicStream.endWriteEncaps方法，大约344行，写此后（39位后的）的数据长度，总数据长度减去39
-	buf.Write(utils.IntToBytes(real)) //修正数据长度，是总长度减去写完context后的长度 39 +4 = 43
-
-	buf.WriteByte(1) //encoding major 43+1=44
-	buf.WriteByte(1) //encoding minor 44+1=45
-
-	//buf.WriteStr("::service::HelloService") //45+1+23=69,只要字节数量一致就可以
-	//::module名（slice中定义的）::服务名（slice中定义的interface）
-	buf.WriteStr("::" + config.ConfigMap[name][config.Module].(string) + "::" + config.ConfigMap[name][config.Name].(string)) //45+1+23=69
-	//log.Infof("string = [%s]", ("::" + config.ConfigMap[name][config.Module].(string) + "::" + config.ConfigMap[name][config.Name].(string)))
-	//buf.WriteStr("::goiceinter::Goice") //45+1+23=69
-	buf.Flush()
-	head = make([]byte, 14) //先读取头
-	size, err = rw.Read(head)
-	//var __Magic = [4]byte{}
-	//__Magic[0] = head[0]
-	//__Magic[1] = head[1]
-	//__Magic[2] = head[2]
-	//__Magic[3] = head[3]
-	//pmj := head[4]
-	//pmn := head[5]
-	//emj := head[6]
-	//emn := head[7]
-	//rmsg := head[8]
-	//zip := head[9]
-	//fmt.Printf("头 size = %d ,requestId = %d ,msg.size = %d \n", size, utils.BytesToInt(head[10:]), utils.BytesToInt(head[10:]))
-	//fmt.Printf("协议版本major = %d,minor = %d\n", pmj, pmn)
-	//fmt.Printf("编码版本major = %d,minor = %d\n", emj, emn)
-	//fmt.Printf("msg = %d\n", rmsg)
-	//fmt.Printf("压缩标示 = %d\n", zip)
-	//fmt.Printf("数据长度 = %d\n", utils.BytesToInt(head[10:]))
-
-	if err != nil {
-		fmt.Println(size)
-		panic(err)
-	}
-	data = make([]byte, 26) //连接的时候
-	size, err = rw.Read(data)
-	//log.Info("flush reply size : " ,size,utils.BytesToInt32(data[0:4]))
-	//fmt.Printf("requestId = %d,数据长度 = %d\n", utils.BytesToInt(data[14:]),utils.BytesToInt(data[10:]))
-
-	//os.Exit(1)
-}
-
-func (this *Connection) init() {
-	magic := []byte{0x49, 0x63, 0x65, 0x50}
-	var msgType byte = 0
-	requestHdr := []byte{
-		magic[0],
-		magic[1],
-		magic[2],
-		magic[3],
-		GetDefaultProtocolVersion().Major,
-		GetDefaultProtocolVersion().Minor,
-		GetDefaultProtocalEncodingVersion().Major,
-		GetDefaultProtocalEncodingVersion().Minor,
-		msgType,
-		1} // Compression status. 0：不支持压缩，1：支持压缩，2：已经压缩
-	//还要设置压缩位，见requestHdr里面关于压缩位设置。如果压缩的话，第10位设置为2，并且11 12 13 14设置为压缩后的长度。
-	var facet string
-	rw := bufio.NewReadWriter(bufio.NewReader(this), bufio.NewWriter(this))
-	var buf = NewIceBuff(rw)
-	var identity Identity
-	var context map[string]string
-	var mode byte
-	//var pmj, pmn, emj, emn, zip, rmsg byte
-	//var requestId int
-	var size int
-	var operator string
-	var head, data []byte
-	var err error
-	buf.Write(requestHdr)           // 10字节
-	buf.Write(utils.IntToBytes(69)) //size 10 +4 = 14
-	buf.Write(utils.IntToBytes(1))  //requestId 14+4=18
-	identity = Identity{
-		name: "HelloIce",
-	}
-	buf.WriteStr(identity.name)     //18+1+8=27
-	buf.WriteStr(identity.category) //27+1=28
-
-	if len(facet) == 0 {
-		buf.WriteByte(0) //28+1=29
-	} else {
-		facets := []string{facet}
-		buf.WriteStringArray(facets)
-	}
-	operator = "ice_isA"
-	buf.WriteStr(operator) //29+1+7=37
-	mode = 1
-	buf.WriteByte(mode) //37+1=38
-	context = make(map[string]string)
-	buf.WriteStringMap(context) //38+1=39
-	//数据整形 java 中 BasicStream.endWriteEncaps方法，大约344行，写此后（39位后的）的数据长度，总数据长度减去39
-	buf.Write(utils.IntToBytes(30)) //修正数据长度，是总长度减去写完context后的长度 39 +4 = 43
-
-	buf.WriteByte(1) //encoding major 43+1=44
-	buf.WriteByte(1) //encoding minor 44+1=45
-
-	buf.WriteStr("::service::HelloService") //45+1+23=69
-	buf.Flush()
-
-	head = make([]byte, 14) //先读取头
-	size, err = rw.Read(head)
-	if err != nil {
-		fmt.Println(size)
-		panic(err)
-	}
-	var __Magic = [4]byte{}
-	__Magic[0] = head[0]
-	__Magic[1] = head[1]
-	__Magic[2] = head[2]
-	__Magic[3] = head[3]
-	//pmj = head[4]
-	//pmn = head[5]
-	//emj = head[6]
-	//emn = head[7]
-	//rmsg = head[8]
-	//zip = head[9]
-	//fmt.Printf("头 size = %d ,requestId = %d ,msg.size = %d \n", size, utils.BytesToInt(head[10:]), utils.BytesToInt(head[10:]))
-	//for i, v := range __Magic {
-	//	fmt.Printf("__Magic[%d]=%d\n", i, v)
-	//}
-	//
-	//fmt.Printf("协议版本major = %d,minor = %d\n", pmj, pmn)
-	//fmt.Printf("编码版本major = %d,minor = %d\n", emj, emn)
-	//fmt.Printf("msg = %d\n", rmsg)
-	//fmt.Printf("压缩标示 = %d\n", zip)
-	//fmt.Printf("数据长度 = %d\n", utils.BytesToInt(head[10:]))
-
-	data = make([]byte, 40) //先读取头
-	size, err = rw.Read(data)
-	//requestId = utils.BytesToInt(data[0:4])
-	//fmt.Printf("请求ID = %d\n", requestId)
-	//fmt.Println("================")
-}
+//func (this *Connection) init() {
+//	magic := []byte{0x49, 0x63, 0x65, 0x50}
+//	var msgType byte = 0
+//	requestHdr := []byte{
+//		magic[0],
+//		magic[1],
+//		magic[2],
+//		magic[3],
+//		GetDefaultProtocolVersion().Major,
+//		GetDefaultProtocolVersion().Minor,
+//		GetDefaultProtocalEncodingVersion().Major,
+//		GetDefaultProtocalEncodingVersion().Minor,
+//		msgType,
+//		1} // Compression status. 0：不支持压缩，1：支持压缩，2：已经压缩
+//	//还要设置压缩位，见requestHdr里面关于压缩位设置。如果压缩的话，第10位设置为2，并且11 12 13 14设置为压缩后的长度。
+//	var facet string
+//	rw := bufio.NewReadWriter(bufio.NewReader(this), bufio.NewWriter(this))
+//	var buf = NewIceBuff(rw)
+//	var identity Identity
+//	var context map[string]string
+//	var mode byte
+//	//var pmj, pmn, emj, emn, zip, rmsg byte
+//	//var requestId int
+//	var size int
+//	var operator string
+//	var head, data []byte
+//	var err error
+//	buf.Write(requestHdr)           // 10字节
+//	buf.Write(utils.IntToBytes(69)) //size 10 +4 = 14
+//	buf.Write(utils.IntToBytes(1))  //requestId 14+4=18
+//	identity = Identity{
+//		name: "HelloIce",
+//	}
+//	buf.WriteStr(identity.name)     //18+1+8=27
+//	buf.WriteStr(identity.category) //27+1=28
+//
+//	if len(facet) == 0 {
+//		buf.WriteByte(0) //28+1=29
+//	} else {
+//		facets := []string{facet}
+//		buf.WriteStringArray(facets)
+//	}
+//	operator = "ice_isA"
+//	buf.WriteStr(operator) //29+1+7=37
+//	mode = 1
+//	buf.WriteByte(mode) //37+1=38
+//	context = make(map[string]string)
+//	buf.WriteStringMap(context) //38+1=39
+//	//数据整形 java 中 BasicStream.endWriteEncaps方法，大约344行，写此后（39位后的）的数据长度，总数据长度减去39
+//	buf.Write(utils.IntToBytes(30)) //修正数据长度，是总长度减去写完context后的长度 39 +4 = 43
+//
+//	buf.WriteByte(1) //encoding major 43+1=44
+//	buf.WriteByte(1) //encoding minor 44+1=45
+//
+//	buf.WriteStr("::service::HelloService") //45+1+23=69
+//	buf.Flush()
+//
+//	head = make([]byte, 14) //先读取头
+//	size, err = rw.Read(head)
+//	if err != nil {
+//		fmt.Println(size)
+//		panic(err)
+//	}
+//	var __Magic = [4]byte{}
+//	__Magic[0] = head[0]
+//	__Magic[1] = head[1]
+//	__Magic[2] = head[2]
+//	__Magic[3] = head[3]
+//	//pmj = head[4]
+//	//pmn = head[5]
+//	//emj = head[6]
+//	//emn = head[7]
+//	//rmsg = head[8]
+//	//zip = head[9]
+//	//fmt.Printf("头 size = %d ,requestId = %d ,msg.size = %d \n", size, utils.BytesToInt(head[10:]), utils.BytesToInt(head[10:]))
+//	//for i, v := range __Magic {
+//	//	fmt.Printf("__Magic[%d]=%d\n", i, v)
+//	//}
+//	//
+//	//fmt.Printf("协议版本major = %d,minor = %d\n", pmj, pmn)
+//	//fmt.Printf("编码版本major = %d,minor = %d\n", emj, emn)
+//	//fmt.Printf("msg = %d\n", rmsg)
+//	//fmt.Printf("压缩标示 = %d\n", zip)
+//	//fmt.Printf("数据长度 = %d\n", utils.BytesToInt(head[10:]))
+//
+//	data = make([]byte, 40) //先读取头
+//	size, err = rw.Read(data)
+//	//requestId = utils.BytesToInt(data[0:4])
+//	//fmt.Printf("请求ID = %d\n", requestId)
+//	//fmt.Println("================")
+//}
--- main2.go
+++ main2.go
@@ -2,112 +2,104 @@ package main
 
 //发送40字节的head试试
 
-import (
-	"bufio"
-	"fmt"
-	"github.com/aloxc/goice/ice"
-	"github.com/aloxc/goice/utils"
-	"time"
-)
-
 func main2() {
-	var conn, err = ice.Connect("tcp4", "127.0.0.1:1888", 0)
-	if err != nil { //如果连接失败。则返回。
-		fmt.Println("连接出错：", err)
-	}
-	rw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
-	var facet string
-	var buf = ice.NewIceBuff(rw)
-	var context map[string]string
-	var mode, pmj, pmn, emj, emn, zip, rmsg byte
-	var requestId, size int
-	var head, data []byte
-
-	//time.Sleep(20 * time.Second)
-
-	buf.WriteHead()
-	buf.WriteTotalSize(49)
-	buf.WriteRequestId(3)
-	buf.WriteIdentity(ice.GetIdentity("HelloIce", ""))
-	buf.WriteFacet(facet)
-	buf.WriteOperator("sayHello")
-	mode = 0
-	buf.WriteMode(mode) //38+1=39
-	context = make(map[string]string)
-	buf.WriteContext(context) //39+1=40
-	//需要设置实际长度,这个值是总数据长度减去设置完context后的长度
-	buf.WriteRealSize(9)
-	buf.WriteEncodingVersion(ice.GetDefaultEncodingVersion())
-	buf.WriteStr("aa") //46+1+2=49
-	buf.Flush()
-
-	head = make([]byte, 25) //先读取头
-	size, err = rw.Read(head)
-	var __Magic = [4]byte{}
-	__Magic[0] = head[0]
-	__Magic[1] = head[1]
-	__Magic[2] = head[2]
-	__Magic[3] = head[3]
-	pmj = head[4]
-	pmn = head[5]
-	emj = head[6]
-	emn = head[7]
-	rmsg = head[8]
-	zip = head[9]
-	for i, v := range __Magic {
-		fmt.Printf("__Magic[%d]=%d\n", i, v)
-	}
-	fmt.Printf("协议版本major = %d,minor = %d\n", pmj, pmn)
-	fmt.Printf("编码版本major = %d,minor = %d\n", emj, emn)
-	fmt.Printf("msg = %d\n", rmsg)
-	fmt.Printf("压缩标示 = %d\n", zip)
-	fmt.Printf("数据长度 = %d\n", utils.BytesToInt(head[10:14]))
-	requestId = utils.BytesToInt(head[14:18])
-	fmt.Printf("请求ID = %d\n", requestId)
-	var replyStatus uint8 = head[18]
-	fmt.Println("响应状态 ", replyStatus)
-	var lastSize = utils.BytesToInt(head[19:23])
-	fmt.Println("整形后面的数据长度（包括整形4字节） ", lastSize)
-	_encodingMajor := head[23]
-	_encodingMinor := head[24]
-	lastSize = lastSize - 4 - 1 - 1 //4:整形后面包括整形长度，1：主编码版本 ，1：副编码版本
-	fmt.Printf("编码版本major = %d,minor = %d\n", _encodingMajor, _encodingMinor)
-	fmt.Println("最终数据长度及数据 的长度", lastSize)
-	sizeDefine := 0
-	realSize := 0
-	if lastSize <= 255 {
-		//读取一个字节
-		sizeDefine = 1
-		dataSizeData := make([]byte, sizeDefine)
-		size, err = rw.Read(dataSizeData)
-		realSize = int(dataSizeData[0])
-	} else {
-		//读取一个字节-1，跟着4个字节的数据长度（int）
-		sizeDefine = 5
-		dataSizeData := make([]byte, sizeDefine)
-		size, err = rw.Read(dataSizeData)
-		realSize = utils.BytesToInt(dataSizeData[1:])
-		fmt.Println("长度超过254，读取下这个 -1 是什么 %V", dataSizeData[0])
-	}
-	lastSize = lastSize - sizeDefine
-	fmt.Println("计算数据长度是 ", lastSize)
-	fmt.Println("真实数据长度是 ", realSize)
-	data = make([]byte, lastSize) //先读取头
-	size, err = rw.Read(data)
-	fmt.Println("剩余数据长度 = ", size)
-	//fmt.Printf("执行结果[]\n", string(data[:]))
-	for in, d := range data {
-		fmt.Printf("执行结果[%d][%d]\n", in, d)
-	}
-	//响应头部10字节 49 63 65 50 01 00 01 00 02 00  // 10
-	//数据总长度， 1D 00 00 00   //10+4 = 14
-	//请求id   02 00 00 00   //14+4 = 18
-	// 一个byte的零 00       //18+1=19   java代码ReplyStatus.replyOK,如果异常就是ReplyStatus.replyUserException
-	// 一个int的零 数据长度，最终会整形的（整形是总的数据长度去掉这个位置之前的长度） 0A 00 00 00         //19 + 4 = 23
-	// 加上encodingVersion  01 01     //23+2 = 25
-	//实际数据
-	//数据长度  03
-	//数据   62 62 62
-	//fmt.Println("数据",s)
-	time.Sleep(2 * time.Second)
+	//var conn, err = ice.Connect("tcp4", "127.0.0.1:1888", 0)
+	//if err != nil { //如果连接失败。则返回。
+	//	fmt.Println("连接出错：", err)
+	//}
+	//rw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+	//var facet string
+	//var buf = ice.NewIceBuff(rw)
+	//var context map[string]string
+	//var mode, pmj, pmn, emj, emn, zip, rmsg byte
+	//var requestId, size int
+	//var head, data []byte
+	//
+	////time.Sleep(20 * time.Second)
+	//
+	//buf.WriteHead()
+	//buf.WriteTotalSize(49)
+	//buf.WriteRequestId(3)
+	//buf.WriteIdentity(ice.GetIdentity("HelloIce", ""))
+	//buf.WriteFacet(facet)
+	//buf.WriteOperator("sayHello")
+	//mode = 0
+	//buf.WriteMode(mode) //38+1=39
+	//context = make(map[string]string)
+	//buf.WriteContext(context) //39+1=40
+	////需要设置实际长度,这个值是总数据长度减去设置完context后的长度
+	//buf.WriteRealSize(9)
+	//buf.WriteEncodingVersion(ice.GetDefaultEncodingVersion())
+	//buf.WriteStr("aa") //46+1+2=49
+	//buf.Flush()
+	//
+	//head = make([]byte, 25) //先读取头
+	//size, err = rw.Read(head)
+	//var __Magic = [4]byte{}
+	//__Magic[0] = head[0]
+	//__Magic[1] = head[1]
+	//__Magic[2] = head[2]
+	//__Magic[3] = head[3]
+	//pmj = head[4]
+	//pmn = head[5]
+	//emj = head[6]
+	//emn = head[7]
+	//rmsg = head[8]
+	//zip = head[9]
+	//for i, v := range __Magic {
+	//	fmt.Printf("__Magic[%d]=%d\n", i, v)
+	//}
+	//fmt.Printf("协议版本major = %d,minor = %d\n", pmj, pmn)
+	//fmt.Printf("编码版本major = %d,minor = %d\n", emj, emn)
+	//fmt.Printf("msg = %d\n", rmsg)
+	//fmt.Printf("压缩标示 = %d\n", zip)
+	//fmt.Printf("数据长度 = %d\n", utils.BytesToInt(head[10:14]))
+	//requestId = utils.BytesToInt(head[14:18])
+	//fmt.Printf("请求ID = %d\n", requestId)
+	//var replyStatus uint8 = head[18]
+	//fmt.Println("响应状态 ", replyStatus)
+	//var lastSize = utils.BytesToInt(head[19:23])
+	//fmt.Println("整形后面的数据长度（包括整形4字节） ", lastSize)
+	//_encodingMajor := head[23]
+	//_encodingMinor := head[24]
+	//lastSize = lastSize - 4 - 1 - 1 //4:整形后面包括整形长度，1：主编码版本 ，1：副编码版本
+	//fmt.Printf("编码版本major = %d,minor = %d\n", _encodingMajor, _encodingMinor)
+	//fmt.Println("最终数据长度及数据 的长度", lastSize)
+	//sizeDefine := 0
+	//realSize := 0
+	//if lastSize <= 255 {
+	//	//读取一个字节
+	//	sizeDefine = 1
+	//	dataSizeData := make([]byte, sizeDefine)
+	//	size, err = rw.Read(dataSizeData)
+	//	realSize = int(dataSizeData[0])
+	//} else {
+	//	//读取一个字节-1，跟着4个字节的数据长度（int）
+	//	sizeDefine = 5
+	//	dataSizeData := make([]byte, sizeDefine)
+	//	size, err = rw.Read(dataSizeData)
+	//	realSize = utils.BytesToInt(dataSizeData[1:])
+	//	fmt.Println("长度超过254，读取下这个 -1 是什么 %V", dataSizeData[0])
+	//}
+	//lastSize = lastSize - sizeDefine
+	//fmt.Println("计算数据长度是 ", lastSize)
+	//fmt.Println("真实数据长度是 ", realSize)
+	//data = make([]byte, lastSize) //先读取头
+	//size, err = rw.Read(data)
+	//fmt.Println("剩余数据长度 = ", size)
+	////fmt.Printf("执行结果[]\n", string(data[:]))
+	//for in, d := range data {
+	//	fmt.Printf("执行结果[%d][%d]\n", in, d)
+	//}
+	////响应头部10字节 49 63 65 50 01 00 01 00 02 00  // 10
+	////数据总长度， 1D 00 00 00   //10+4 = 14
+	////请求id   02 00 00 00   //14+4 = 18
+	//// 一个byte的零 00       //18+1=19   java代码ReplyStatus.replyOK,如果异常就是ReplyStatus.replyUserException
+	//// 一个int的零 数据长度，最终会整形的（整形是总的数据长度去掉这个位置之前的长度） 0A 00 00 00         //19 + 4 = 23
+	//// 加上encodingVersion  01 01     //23+2 = 25
+	////实际数据
+	////数据长度  03
+	////数据   62 62 62
+	////fmt.Println("数据",s)
+	//time.Sleep(2 * time.Second)
 }
